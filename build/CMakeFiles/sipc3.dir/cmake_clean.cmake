9a95897a9fb45f5206ec953f331ee5edb7ab6324
Àngel Julián Bolaño Campos <alu0101139456@ull.edu.es>
2020-12-14 17:23:48 +0000

    Añadida compilación con CMake

---
+++ build/CMakeFiles/sipc3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sipc3.dir/main.cpp.o"
+  "sipc3"
+  "sipc3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sipc3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
