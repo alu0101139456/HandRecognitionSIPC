595d183a8dfe0a6c260301e9bc632ecc10f68bd6
Àngel Julián Bolaño Campos <alu0101139456@ull.edu.es>
2020-12-17 11:13:10 +0000

    Update

--- main.cpp
+++ main.cpp
@@ -11,9 +11,11 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	Mat frame,roi;
+	Mat frame,roi,fgMask;
 	VideoCapture cap;
 	cap.open(0);
+
+  Ptr<BackgroundSubtractor> pBackSub=createBackgroundSubtractorMOG2();
 
 	if (!cap.isOpened()) {
 		printf("Error opening cam\n");
@@ -21,24 +23,31 @@ int main(int argc, char* argv[])
 	}
 	namedWindow("Frame");
 	namedWindow("ROI");
+	namedWindow("Foreground Mask");
+
 
 	Rect rect(400,100,200,200);	
+	bool st = true;
 	while(true) {
 
 		cap>>frame;
 		flip(frame,frame,1);
 
 		frame(rect).copyTo(roi);
-		//roi = frame(rect);
+		if (st) {
+			pBackSub->apply(roi,fgMask);
+		}//roi = frame(rect);
 		
 		rectangle(frame, rect,Scalar(255,0,0));
 
 		imshow("Frame",frame);
 		imshow("ROI",roi);
+		imshow("Foreground Mask", fgMask);
 
 		int c = waitKey(40);
 
 		if ((char)c =='q') break;
+		if ((char)c =='s') st = false;
 	}
 	cap.release();
 	destroyAllWindows();
