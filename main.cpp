52665d356e558e6c308e0e71a432544b7f9bff53
Àngel Julián Bolaño Campos <alu0101139456@ull.edu.es>
2020-12-21 23:03:23 +0000

    update

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 
 int main(int argc, char* argv[])
 {
-	cv::Mat frame, roi, fgMask,gray;
+	cv::Mat frame, roi, fgMask,gray, image;
 	cv::VideoCapture cap;
 	cap.open(0);
 
@@ -26,7 +26,7 @@ int main(int argc, char* argv[])
 	cv::namedWindow("Foreground Mask");
 
 	cv::Rect rect(400,100,200,200);	
-
+int index=-1;
 int max = 0;
 	double lr = -1;
 	while(true) {
@@ -39,32 +39,37 @@ int max = 0;
 		
 		cv::cvtColor(roi, gray, cv::COLOR_RGB2GRAY); //Aplicamos a roi escala de grises
 
-		pBackSub->apply(gray,fgMask,lr); //aplico a roi la substracción de fondo
-				
 		rectangle(frame, rect,cv::Scalar(255,0,0)); //Dibujo en FRAME el rectanglo
 		
-		
-		cv::threshold(gray, gray, 127, 255, cv::THRESH_BINARY); //sólo se usa para el tutorial
+		cv::threshold(gray, gray, 127, 255, cv::THRESH_BINARY); //Se puede quitar, se consigue mayor contraste
 
-		cv::findContours(gray, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+		pBackSub->apply(gray,fgMask,lr); //aplico a gray la substracción de fondo
+		
+		cv::threshold(fgMask, image, 127, 255, cv::THRESH_BINARY); //Mandamos gfMask para que convierta a binario
+
+
+		cv::findContours(image, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE); 
 	  cv::drawContours(roi, contours, -1, cv::Scalar(0,255,0),3);
 		
-		std::vector<std::vector<cv::Point> > hull(contours.size());
-		for (size_t i = 0; i < contours.size(); i++)
-			 cv::convexHull(contours[i], hull[i]);
-		// cv::drawContours(roi, hull, -1, cv::Scalar(255,0,0),2); //sólo se usa para el tutorial
+		std::vector<std::vector<cv::Point> > malla(contours.size());
+		for (size_t i = 0; i < contours.size(); i++) {
+			cv::convexHull(contours[i], malla[i]);
+
+		}
+		cv::drawContours(roi, malla, -1, cv::Scalar(255,0,0),2); //sólo se usa para el tutorial
 		
-		cv::drawContours(roi, hull, -1, cv::Scalar(255, 0, 0), 2, 8, std::vector<cv::Vec4i>(), 0, cv::Point()); 
+		// cv::drawContours(roi, malla, -1, cv::Scalar(255, 0, 0), 2, 8, std::vector<cv::Vec4i>(), 0, cv::Point()); 
 
 		cv::imshow("Frame",frame);
 		cv::imshow("ROI",roi);
 		cv::imshow("Foreground Mask", fgMask);
 		cv::imshow("gray", gray);
+		// cv::imshow("image", image);
 		int c = cv::waitKey(40);
 
 		if ((char)c =='q') break;
 		// if ((char)c =='s') lr = 0;
-		if ((char)c =='l') (lr = -1)? lr = 0: lr = -1;  //Se usa para dejar de "aprender" el fondo cambiando el valor del LearningRate
+		if ((char)c =='l') lr = 0;  //Se usa para dejar de "aprender" el fondo cambiando el valor del LearningRate
 
 	}
 	cap.release();
