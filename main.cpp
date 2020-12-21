02a03852e1d0a76b98a664986131dbcb14db03dc
Àngel Julián Bolaño Campos <alu0101139456@ull.edu.es>
2020-12-21 19:36:50 +0000

    update

--- main.cpp
+++ main.cpp
@@ -5,50 +5,70 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/video.hpp>
 
-using namespace cv;
-using namespace std;
+
 
 
 int main(int argc, char* argv[])
 {
-	Mat frame,roi,fgMask;
-	VideoCapture cap;
+	cv::Mat frame, roi, fgMask,gray;
+	cv::VideoCapture cap;
 	cap.open(0);
 
-  Ptr<BackgroundSubtractor> pBackSub=createBackgroundSubtractorMOG2();
+  cv::Ptr<cv::BackgroundSubtractor> pBackSub = cv::createBackgroundSubtractorMOG2();
+	std::vector<std::vector<cv::Point> > contours;
 
 	if (!cap.isOpened()) {
-		printf("Error opening cam\n");
+		std::cerr << "No se puede abrir camara" << std::endl;
 		return -1;
 	}
-	namedWindow("Frame");
-	namedWindow("ROI");
-	namedWindow("Foreground Mask");
+	cv::namedWindow("Frame");
+	cv::namedWindow("ROI");
+	cv::namedWindow("Foreground Mask");
 
+	cv::Rect rect(400,100,200,200);	
 
-	Rect rect(400,100,200,200);	
-	bool st = true;
+int max = 0;
+	double lr = -1;
 	while(true) {
 
-		cap>>frame;
-		flip(frame,frame,1);
+		cap>>frame; //Capturo las imagenes al objeto FRAME
+		flip(frame,frame,1); //Rota Horizontalmente para efecto espejo
 
-		frame(rect).copyTo(roi);
-		if (st) {
-			pBackSub->apply(roi,fgMask);
-		}//roi = frame(rect);
+		frame(rect).copyTo(roi); //Copio en roi el recuadro que usaremos
+		cv::Point centro(10, 10);
 		
-		rectangle(frame, rect,Scalar(255,0,0));
+		cv::cvtColor(roi, gray, cv::COLOR_RGB2GRAY); //Aplicamos a roi escala de grises
 
-		imshow("Frame",frame);
-		imshow("ROI",roi);
-		imshow("Foreground Mask", fgMask);
+		pBackSub->apply(gray,fgMask,lr); //aplico a roi la substracción de fondo
+				
+		rectangle(frame, rect,cv::Scalar(255,0,0)); //Dibujo en FRAME el rectanglo
+		
+		
+		cv::threshold(gray, gray, 127, 255, cv::THRESH_BINARY); //sólo se usa para el tutorial
 
-		int c = waitKey(40);
+		cv::findContours(gray, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	  cv::drawContours(roi, contours, -1, cv::Scalar(0,255,0),3);
+		
+		std::vector<std::vector<cv::Point> > hull(contours.size());
+		for (size_t i = 0; i < contours.size(); i++)
+			 cv::convexHull(contours[i], hull[i]);
+		// cv::drawContours(roi, hull, -1, cv::Scalar(255,0,0),2); //sólo se usa para el tutorial
+		
+		cv::drawContours(roi, hull, -1, cv::Scalar(255, 0, 0), 2, 8, std::vector<cv::Vec4i>(), 0, cv::Point()); 
+
+		cv::imshow("Frame",frame);
+		cv::imshow("ROI",roi);
+		cv::imshow("Foreground Mask", fgMask);
+		cv::imshow("gray", gray);
+		int c = cv::waitKey(40);
 
 		if ((char)c =='q') break;
-		if ((char)c =='s') st = false;
+		// if ((char)c =='s') lr = 0;
+		if ((char)c =='l') (lr = -1)? lr = 0: lr = -1;  //Se usa para dejar de "aprender" el fondo cambiando el valor del LearningRate
+
 	}
 	cap.release();
-	destroyAllWindows();
+	cv::destroyAllWindows();
+	
+
 }
